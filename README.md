e646d59586807f4e06838257abdbb6631d70ba8d
James Shi <jamesshi@airbears2-10-142-62-239.airbears2.1918.berkeley.edu>
2019-11-05 19:08:29 -0800

    Added floating assignment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Self-learning a C++ online class from MIT OCW
+
 https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-s096-effective-programming-in-c-and-c-january-iap-2014/index.htm
---
+++ floating/floating.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+union float_bits
+{
+    float f;
+    unsigned int bits;
+};
+
+void print_hex(float f)
+{
+    union float_bits t;
+    t.f = f;
+    printf("The float looks like 0x%x in hex.\n", t.bits);
+}
+
+void print_repr(float f)
+{
+    union float_bits t;
+    t.f = f;
+    if (t.bits & 1U << 31)
+        printf("-");
+    printf("1.");
+    for (int shift = 22; shift >= 0; shift--)
+        printf("%d", !!(t.bits & 1U << shift));
+    printf(" * 2^");
+    int exponent = (int)(t.bits << 1 >> 24);
+    printf("%d\n", exponent - 127);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while (n--)
+    {
+        float f;
+        scanf("%f", &f);
+        print_repr(f);
+    }
+    return 0;
+}
