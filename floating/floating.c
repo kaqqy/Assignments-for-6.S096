e6fb4b35ec1fc02c42ba3cf6d5fe3c9deafc9ec9
James Shi <jamesshi@airbears2-10-142-62-239.airbears2.1918.berkeley.edu>
2019-11-05 19:20:53 -0800

    changed the code style for floating.c

--- floating/floating.c
+++ floating/floating.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-union float_bits
-{
+union float_bits {
     float f;
     unsigned int bits;
 };
 
-void print_hex(float f)
-{
+void print_hex(float f) {
     union float_bits t;
+
     t.f = f;
     printf("The float looks like 0x%x in hex.\n", t.bits);
 }
 
-void print_repr(float f)
-{
+void print_repr(float f) {
     union float_bits t;
+
     t.f = f;
     if (t.bits & 1U << 31)
         printf("-");
@@ -27,12 +26,11 @@ void print_repr(float f)
     printf("%d\n", exponent - 127);
 }
 
-int main()
-{
+int main() {
     int n;
+
     scanf("%d", &n);
-    while (n--)
-    {
+    while (n--) {
         float f;
         scanf("%f", &f);
         print_repr(f);
