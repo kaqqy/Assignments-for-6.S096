6c9a9e0d2f40a401c0b1c4d39eb1808c8ddac678
James Shi <jamesshi@airbears2-10-142-35-253.airbears2.1918.berkeley.edu>
2019-11-11 15:14:16 -0800

    added more tests for c linked list

--- list/src/test.c
+++ list/src/test.c
@@ -18,6 +18,21 @@ int main(void) {
   }
 
   list_print( list );
+  list_apply( &list, sq );
+  list_print( list );
+  list_append( &list, list_reduce( &list, plus ) );
+  list_print( list );
+  for (int i = 0; i < 100; i++) {
+    list_insert_before( &list, i - 1, i );
+  }
+  list_print( list );
+  for (int i = 2; i < 20; i++) {
+    list_delete( &list, i );
+  }
+  list_print( list );
+  list_delete( &list, 0 );
+  list_delete( &list, -1 );
+  list_print( list );
   list_clear( &list );
   return 0;
 }
